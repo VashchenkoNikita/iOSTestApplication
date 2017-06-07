c503f059d46bbb55142303b3b784930a0caa0baf
test <test@test.com>
2017-06-07 14:14:53 +0300

    Task 1 : log in with facebook

--- TestProject/TestProject/ViewController.swift
+++ TestProject/TestProject/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    // Add a custom login button to your app
+    
+    let myLoginButton = UIButton(type: .custom)
+    myLoginButton.backgroundColor = UIColor.blue
+    myLoginButton.frame = CGRect(x: 0, y: 0, width: 180, height: 40)
+    myLoginButton.center = view.center
+    myLoginButton.setTitle("Log in with Facebook", for: .normal)
+    
+    // Handle clicks on the button
+    myLoginButton.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
+    
+    // Add the button to the view
+    view.addSubview(myLoginButton)
+  }
+  
+  // Once the button is clicked, show the login dialog
+  @objc func loginButtonClicked() {
+    let loginManager = LoginManager()
+    loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+      switch loginResult {
+      case .failed(let error):
+        print(error)
+      case .cancelled:
+        print("User cancelled login.")
+      case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+        print("Logged in!")
+      }
 }
-
+}
+}
